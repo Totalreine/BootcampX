85935fbe66f0c4d7d6028a08d5ef99337c308707
Juan Escobar <jcarriola83@gmail.com>
2023-02-09 22:47:22 +0000

    students and teachers queries whit node-postgres

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,24 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+
+pool.query(`
+SELECT DISTINCT teachers.name as teacher_name, cohorts.name as cohort_name
+FROM teachers
+JOIN assistance_requests ON assistance_requests.teacher_id = teachers.id
+JOIN students ON students.id = assistance_requests.student_id
+JOIN cohorts ON cohorts.id = students.cohort_id
+WHERE cohorts.name = '${process.argv[2]}'
+ORDER BY teacher_name;
+`)
+.then(res => {
+  res.rows.forEach(user => {
+    console.log(`${user.cohort_name}: ${user.teacher_name}`);
+  })
+}).catch(err => console.error('query error', err.stack));
