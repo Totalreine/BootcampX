99197e4756a092cbeaecd6aa72a6dc9a65f84592
Juan <jcarriola83@gmail.com>
2023-02-09 21:16:23 -0800

    sanitizing of the user inputs

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,4 +1,5 @@
 const { Pool } = require('pg');
+const client = require('pg/lib/native/client');
 
 const pool = new Pool({
   user: 'vagrant',
@@ -7,16 +8,20 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-
-pool.query(`
+const text = `
 SELECT DISTINCT teachers.name as teacher_name, cohorts.name as cohort_name
 FROM teachers
 JOIN assistance_requests ON assistance_requests.teacher_id = teachers.id
 JOIN students ON students.id = assistance_requests.student_id
 JOIN cohorts ON cohorts.id = students.cohort_id
-WHERE cohorts.name = '${process.argv[2]}'
+WHERE cohorts.name = $1
 ORDER BY teacher_name;
-`)
+`
+const cohortName = process.argv[2];
+const values = [`${cohortName}`]
+
+pool
+.query(text, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.cohort_name}: ${user.teacher_name}`);
